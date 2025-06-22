25b09b0fb94929060d368f62bc1bab26cfb01ad5
Le Hazmel <hazem.g.elmahy@gmail.com>
2025-06-22 10:54:37 +0000

    added supermaven

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -3,3 +3,8 @@ vim.g.mapleader = " "
 local keymap = vim.keymap.set
 keymap("n", "<leader>ff", require("telescope.builtin").find_files)
 keymap("n", "<C-n>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+vim.keymap.set("n", "<leader>j", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "<leader>k", vim.diagnostic.goto_next)
+vim.keymap.set("n", "K", vim.lsp.buf.hover, { noremap = true, silent = true })
+
