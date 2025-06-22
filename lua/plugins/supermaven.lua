d39f4e029a50b17215c568d90cba52aadd8f6c3d
Le Hazmel <hazem.g.elmahy@gmail.com>
2025-06-22 11:16:19 +0000

    fixed supermaven conflicting keymap

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -1,6 +1,17 @@
 return {
       "supermaven-inc/supermaven-nvim",
       config = function()
-        require("supermaven-nvim").setup({})
+        require("supermaven-nvim").setup({
+          keymaps = {
+            accept_selection = "<Tab>",
+            accept_word = "<C-l>",
+            clear_suggestions = "<C-h>",
+          },
+          color = {
+            suggestion_color = "#eeeeee",
+            cterm = 230,
+          },
+          disable_keymaps = false,
+        })
       end,
   }
