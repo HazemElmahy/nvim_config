e699c0ace1128edfedc56d1943140187b82ac887
Le Hazmel <hazem.g.elmahy@gmail.com>
2025-07-15 13:44:27 +0000

    changed some mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,7 +4,11 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
+-- map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+
+-- Unmap NvChad's C-h in insert mode to restore default backspace behavior
+vim.keymap.del("i", "<C-h>")
+vim.keymap.del("i", "<C-j>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
